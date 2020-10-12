d769ad412db6b1ee8460a3626564c070be8057e7
GBSO <m93developer@gmail.com>
2020-10-11 20:41:43 -0500

    Se Agrega nuevo proyecto asp.net mvc core en clanco con compatibilidad para doker

---
+++ Gbso.App.NetMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Gbso.App.NetMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
