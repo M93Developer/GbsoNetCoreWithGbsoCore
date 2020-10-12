ea0ad15a5a2399c10d103efeb136a427e7f25a53
GBSO <m93developer@gmail.com>
2020-10-11 23:14:06 -0500

    Nuevas tablas en sql

---
+++ Gbso.App.Mssql/General/ModelState.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [General].[ModelState]
+(
+	[Key] SMALLINT NOT NULL PRIMARY KEY, 
+    [Description] VARCHAR(10) NULL
+)
---
+++ Gbso.App.Mssql/General/Person.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [General].[Person]
+(
+	[Key] INT NOT NULL PRIMARY KEY, 
+    [State] SMALLINT NOT NULL, 
+    [TimeStamp] TIMESTAMP NOT NULL, 
+    [IpLastChange] VARCHAR(50) NULL, 
+    [UserLastChange] INT NULL, 
+    CONSTRAINT [FK_Person_To_ModelState] FOREIGN KEY ([State]) REFERENCES [General].[ModelState]([Key])
+)
